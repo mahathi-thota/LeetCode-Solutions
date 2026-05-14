96b9f077bfe4610194f201db9c410b14daa9be5f
thotamahathi <thotamahathi1@gmail.com>
2026-05-14 17:56:31 +0530

    Time: 0 ms (100%), Space: 44.4 MB (91.62%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,33 +1,20 @@
 class Solution {
+
     public boolean isGood(int[] nums) {
-        int n = 0;
-        for (int num : nums) {
-            if (num > n) n = num; // find max element
-        }
-
-        // Length must be n + 1
-        if (nums.length != n + 1) {
-            return false;
-        }
-
-        // Frequency array (values up to 200)
-        int[] freq = new int[201];
-        for (int num : nums) {
-            freq[num]++;
-        }
-
-        // Check 1..n-1 appear exactly once
-        for (int i = 1; i < n; i++) {
-            if (freq[i] != 1) {
+        int n = nums.length;
+        int[] count = new int[n];
+        for (int a : nums) {
+            if (a >= n) {
                 return false;
             }
+            if (a < n - 1 && count[a] > 0) {
+                return false;
+            }
+            if (a == n - 1 && count[a] > 1) {
+                return false;
+            }
+            count[a]++;
         }
-
-        // Check n appears exactly twice
-        if (freq[n] != 2) {
-            return false;
-        }
-
         return true;
     }
 }
