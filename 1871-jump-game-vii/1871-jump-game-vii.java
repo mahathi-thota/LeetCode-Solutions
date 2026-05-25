1230f97c3fdc965042dabbf018ea258d777d7681
thotamahathi <thotamahathi1@gmail.com>
2026-05-25 14:43:52 +0530

    Time: 10 ms (64.48%), Space: 48.1 MB (38.9%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.java
+++ 1871-jump-game-vii/1871-jump-game-vii.java
@@ -1,18 +1,23 @@
 class Solution {
+
     public boolean canReach(String s, int minJump, int maxJump) {
         int n = s.length();
-        int[] pre = new int[n + 1];
-        pre[1] = 1;
-        boolean[] f = new boolean[n];
-        f[0] = true;
-        for (int i = 1; i < n; ++i) {
-            if (s.charAt(i) == '0') {
-                int l = Math.max(0, i - maxJump);
-                int r = i - minJump;
-                f[i] = l <= r && pre[r + 1] - pre[l] > 0;
-            }
-            pre[i + 1] = pre[i] + (f[i] ? 1 : 0);
+        int[] f = new int[n];
+        int[] pre = new int[n];
+        f[0] = 1;
+        // since we start dynamic programming from i=minJump, we need to precompute the prefix sums for the part [0, minJump)
+        for (int i = 0; i < minJump; i++) {
+            pre[i] = 1;
         }
-        return f[n - 1];
+        for (int i = minJump; i < n; i++) {
+            int left = i - maxJump;
+            int right = i - minJump;
+            if (s.charAt(i) == '0') {
+                int total = pre[right] - (left <= 0 ? 0 : pre[left - 1]);
+                f[i] = total != 0 ? 1 : 0;
+            }
+            pre[i] = pre[i - 1] + f[i];
+        }
+        return f[n - 1] == 1;
     }
 }
