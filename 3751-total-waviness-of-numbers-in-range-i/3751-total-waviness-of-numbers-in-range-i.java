e0e8b426cbc783225a67a44309c5543109394bad
thotamahathi <thotamahathi1@gmail.com>
2026-06-04 16:43:35 +0530

    Time: 16 ms (88.1%), Space: 46.1 MB (80.67%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.java
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.java
@@ -1,37 +1,29 @@
 class Solution {
     public int totalWaviness(int num1, int num2) {
-        int sum = 0;
-        for (int i = num1; i <= num2; i++) {
-            sum += waviness(i);
+        int ans = 0;
+        for (int x = num1; x <= num2; x++) {
+            ans += f(x);
         }
-        return sum;
+        return ans;
     }
 
-    private int waviness(int num) {
-        // Collect digits into an array (least significant → most significant)
-        int[] digits = new int[6]; // since num ≤ 10^5, max 6 digits
-        int len = 0;
-        int temp = num;
-        while (temp > 0) {
-            digits[len++] = temp % 10;
-            temp /= 10;
+    private int f(int x) {
+        int[] nums = new int[20];
+        int m = 0;
+        while (x > 0) {
+            nums[m++] = x % 10;
+            x /= 10;
         }
-
-        if (len < 3) return 0; // fewer than 3 digits → no peaks/valleys
-
-        int count = 0;
-        // Traverse from second digit to second-last digit
-        for (int i = len - 2; i >= 1; i--) {
-            int prev = digits[i + 1]; // left neighbor
-            int curr = digits[i];     // current digit
-            int next = digits[i - 1]; // right neighbor
-
-            if (curr > prev && curr > next) {
-                count++; // peak
-            } else if (curr < prev && curr < next) {
-                count++; // valley
+        if (m < 3) {
+            return 0;
+        }
+        int s = 0;
+        for (int i = 1; i < m - 1; i++) {
+            if ((nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
+                || (nums[i] < nums[i - 1] && nums[i] < nums[i + 1])) {
+                s++;
             }
-        }
-        return count;
+         }
+        return s;
     }
 }
