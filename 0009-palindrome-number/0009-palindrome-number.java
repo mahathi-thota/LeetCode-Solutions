48e30d6bebf7b37130e569fb0266875f5cf03d83
mahathi-thota <thotamahathi1@gmail.com>
2026-07-24 17:23:06 +0530

    Time: 6 ms (21.98%), Space: 46 MB (53.52%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -2,10 +2,7 @@ class Solution {
     public boolean isPalindrome(int x) {
         int s=x;
         if(x<0)
-
-        {
             return false;
-        }
         int reverse = 0;
         while(x!=0)
         {
